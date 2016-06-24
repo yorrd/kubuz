6abc83de489a469c04b35ecfc4de2c3d1e2e19b2
Sandlaus <kriege@o2online.de>
2016-06-24 06:47:39 +0200

    Update Kubus.java
    kommentiert, lvl object

--- src/Kubus.java
+++ src/Kubus.java
@@ -2,7 +2,7 @@
  * 
  * 
  * @author Sebastian Kriege
- * Damit die Ümläüte öch rüchtüg gäyn
+ * 
  */
 
 
@@ -10,27 +10,44 @@
 public class Kubus implements Renderable {
 
     private int renderDepth = 15;
-    private int numEdges = 5;
-    private int segments = 4;
+    private int numEdges;
+    private int segments;
     private float radius = 2f;
     private float z_distance = -0.2f;
+    private int rotation = 0;
+    private int position = 0;
 
     private float[] vertexArray;
     private float[] textureArray;
     private int[] indexArray;
+	private boolean[][] curr_level;
+    private Level stage;
     
+	
+	// Constructor
+	public Kubus(int segments, int numEdges){
+		this.segments = segments;
+		this.numEdges = numEdges;
+    	indexArray = new int[6  * (numEdges * segments + 1) * renderDepth];
+    	vertexArray = new float[3  * numEdges * segments * renderDepth];
+    	stage = new Level(segments, numEdges);
+    	curr_level = stage.get_level();
+	}
+	
+	
+	// rename to create geometry
 	@Override
 	public void render() {
 		create_initial();
-		indexArray = create_index();
+		create_index();
 		create_texture();
 		
 	}
-
+	
+	// creates one segment, moves it in the right z position and copies it into vertexArray
     public void create_initial() {
        	float[] segmentArray = new float[3  * numEdges * segments];
     	segmentArray = create_segment(0f);
-    	vertexArray = new float[segmentArray.length * renderDepth];
        	int index_vertex = 0;
         for(int i = 0; i < renderDepth; i ++) {
         	segmentArray = changeZ(z_distance, segmentArray); 
@@ -40,17 +57,18 @@ public class Kubus implements Renderable {
         }
     }
 
+    // creating one segment
     public float[] create_segment(float offset_z) {
     	float[] step = new float[2];
     	float[] vertex_segment = new float[3  * numEdges * segments];
     	float angle = 2 * (float) Math.PI / numEdges;
     	int index = 0;
-    	// erstellt ersten Eckpunkt in richtiger Position 270° + halber winkel
+    	// creates first point in right position, 270° + half angle
         for(int edge = 0; edge < numEdges; edge ++) {
         	vertex_segment[index++] = (float) (radius * Math.cos(1.5f * Math.PI + angle / 2 - angle * edge));
         	vertex_segment[index++] = (float) (radius * Math.sin(1.5f * Math.PI + angle / 2 - angle * edge));
         	vertex_segment[index++] = offset_z;
-        	// für die Schrittweite wird der naechste Eckpunkt berechnet und der Abstand durch Anzahl segmente geteilt
+        	// calculates the difference to the next point
         	step[0] = ((float) (radius * Math.cos(1.5f * Math.PI + angle / 2 - angle * (edge + 1)))
         				- vertex_segment[index - 3]) / segments;
         	step[1] = ((float) (radius * Math.sin(1.5f * Math.PI + angle / 2 - angle * (edge + 1)))
@@ -64,44 +82,36 @@ public class Kubus implements Renderable {
 		return vertex_segment;
     }
     
-    public int[] create_index() {
-    	int[] indexArray = new int[6  * (numEdges * segments + 1) * renderDepth];
+    // creates indexArray
+    public void create_index() {
 		int index = 0;
 		for (int j = 0; j < renderDepth - 1; j++){
 			for (int i = 0; i < (segments * numEdges + 1); i++){
-					indexArray[index++] =  j * segments * numEdges + i % (segments * numEdges);
-					indexArray[index++] = (j + 1) * segments * numEdges + (i + 1) % (segments * numEdges);
-					indexArray[index++] = (j + 1) * segments * numEdges + i % (segments * numEdges);
-					indexArray[index++] = (j + 1) * segments * numEdges + (i + 1) % (segments * numEdges);
-					indexArray[index++] =  j * segments * numEdges + i % (segments * numEdges);
-					indexArray[index++] =  j * segments * numEdges + (i + 1) % (segments * numEdges);
-				}
+				if (curr_level[j + position][i]) continue;
+				indexArray[index++] =  j * segments * numEdges + (i + rotation) % (segments * numEdges);
+				indexArray[index++] = (j + 1) * segments * numEdges + (i + rotation) % (segments * numEdges);
+				indexArray[index++] = (j + 1) * segments * numEdges + (i + 1 + rotation) % (segments * numEdges);
+				indexArray[index++] = (j + 1) * segments * numEdges + (i + 1 + rotation) % (segments * numEdges);
+				indexArray[index++] =  j * segments * numEdges + (i + 1 + rotation) % (segments * numEdges);
+				indexArray[index++] =  j * segments * numEdges + (i + rotation) % (segments * numEdges);
 			}
-		return indexArray;
+		}
     }
     
+    // creates textureArray, textures are mirrored in x-y direction
     public void create_texture() {
     	int index = 0;
         textureArray = new float[2  * numEdges * segments * renderDepth];
     	for(int i = 0; i < renderDepth; i ++) {
-    		if (i % 2 == 0){
             	for(int j = 0; j < numEdges * segments; j ++) {
-            		textureArray[index++] = (j % 2) + 0f;
-            		textureArray[index++] = 0f;
+            		textureArray[index++] = j % 2;
+            		textureArray[index++] = i;
             	}	
-    		}
-    		else {
-            	for(int j = 0; j < numEdges * segments; j ++) {
-            		textureArray[index++] = (j % 2) + 0f;
-            		textureArray[index++] = 1f;
-            	}	
-    		}
-
     	}
 
     }
 
-    // Funktion zum Anpassen der z-Koordinate
+    // adds z offset to an object
     public float[] changeZ(float offset_z, float[] vertex_segment) {
     	for(int i = 2; i < vertex_segment.length; i += 3) {
     		vertex_segment[i] += offset_z;
@@ -109,13 +119,38 @@ public class Kubus implements Renderable {
 		return vertex_segment;
     }
     
-    public void moveZ(float offset_z){
+    // moves the whole tubus in z-direction
+    public void moveZ(float offset_z) {
     	if (vertexArray[2] > 0.5f) {
+        	position++;
+        	create_index();
         	vertexArray = changeZ(z_distance + offset_z, vertexArray);
-        	}
-    	else
-    	{
+        }
+    	else {
         	vertexArray = changeZ(offset_z, vertexArray);
+    	}
+    }
+    
+    // turns the indices, not the vertices
+    public void turn(boolean direction){
+    	if (direction){ // linksdrehung bei true
+    		rotation += segments;
+    		rotation = rotation % (segments * numEdges);
+    	}
+    	else { // rechtsdrehung bei false
+    		rotation -= segments;
+    		rotation = rotation % (segments * numEdges);
+    	}
+    	if (rotation < 0) rotation += segments * numEdges;
+		create_index();
+    }
+    
+    // checks lvl progress
+    public void progress() {
+    	if (position == 50 + renderDepth){ 
+    		position = 0;
+        	stage.next_level();
+        	curr_level = stage.get_level();
     	}
     }
 
