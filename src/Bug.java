dc8186a73bb245d0943bd6733e115067a294771c
Sandlaus <kriege@o2online.de>
2016-06-27 16:30:45 +0200

    Update Bug.java

--- src/Bug.java
+++ src/Bug.java
@@ -7,10 +7,20 @@ class Bug extends Renderable{
 	private float pos_x = 0f; // bug must be able to move x-way
 	private float pos_z = 0.2f; // z-position
 	private float z_size = -0.01f;
+	private float[] body = { 
+			0.0f, 0.3f, 0.0f,	
+			0.25f, 0.0f, 0.25f,
+			0.25f, 0.0f, -0.25f,
+			-0.25f, 0.0f, 0.25f,
+			-0.25f, 0.0f, -0.25f,
+			0.0f, -0.3f, 0.0f,			
+	};
 	
 	// Constructor
 	Bug(){
 		this.textureFile = "gdv.png";
+		shaderVFile = "guivertex.glsl";
+		shaderFFile = "guifragment.glsl";
 		init();
 	}
 	
@@ -18,88 +28,85 @@ class Bug extends Renderable{
     	// vertexArray
     	vertexArray = new float[6 * 9 + 6 * 3];
     	float[] tempArray;
-    	int index = 0;
-    	for(int j = 0; j < 3; j++) {
-    		tempArray = createLeg(bodySize, z_size);
-	    	for(int i = 0; i < 9; i++) {
-	    		vertexArray[index++] = tempArray[i]; 
-	    	}
-    		tempArray = createLeg(-bodySize, z_size);
-	    	for(int i = 0; i < 9; i++) {
-	    		vertexArray[index++] = tempArray[i]; 
-	    	}
-	    	z_size += 0.01f;
-    	}
-    	tempArray = createBody(bodySize);
-    	System.out.println(index);    	
+    	float zPos = 0.25f;
+    	int index = 0;    	
     	for(int i = 0; i < 18; i++) {
-    		vertexArray[index++] = tempArray[i]; 
+    		vertexArray[index++] = body[i]; 
+    	}
+		System.out.println("Body");
+    	for(int i = 0; i < 18; i++) {
+    		System.out.println("x:" + vertexArray[i++]);
+    		System.out.println("y:" + vertexArray[i++]);
+    		System.out.println("z:" + vertexArray[i]);
+    	}
+    	for(int j = 0; j < 3; j++) {
+    		tempArray = createLeg(0.25f, zPos);
+	    	for(int i = 0; i < 9; i++) {
+	    		vertexArray[index++] = tempArray[i]; 
+	    	}
+    		tempArray = createLeg(-0.25f, zPos);
+	    	for(int i = 0; i < 9; i++) {
+	    		vertexArray[index++] = tempArray[i]; 
+	    	}
+	    	zPos -= 0.25f;
+    	}
+		System.out.println("Legs");
+    	for(int i = 18; i < vertexArray.length; i++) {
+    		System.out.println("x:" + vertexArray[i++]);
+    		System.out.println("y:" + vertexArray[i++]);
+    		System.out.println("z:" + vertexArray[i]);
     	}
     	// texture Array
     	index = 0;
     	textureArray = new float[vertexArray.length];
     	for(int i = 0; i < vertexArray.length / 3; i++) {
-    		textureArray[index++] = index; 
-    		textureArray[index++] = index; 
+    		textureArray[index++] = i % 2; 
+    		textureArray[index++] = i % 3; 
     	}
     	// indexArray
-    	indexArray = new int[6 * 6 + 8 * 3];
-    	int temp = 0;
+    	indexArray = new int[24 + 36];
     	index = 0;
-    	for(int i = 0; i < 6 * 2; i++) {
-    		indexArray[index++] = temp++; 
-    		indexArray[index++] = temp++; 
-    		indexArray[index++] = temp++; 
+    	for(int i = 0; i < 4; i++) {
+    		indexArray[index++] = 0; 
+    		indexArray[index++] = 1 + (1 + i) % 4; 
+    		indexArray[index++] = 1 + i % 4; 
     	}
     	for(int i = 0; i < 4; i++) {
-    		indexArray[index++] = 18; 
-    		indexArray[index++] = 19 + (1 + i) % 4; 
-    		indexArray[index++] = 19 + (0 + i) % 4; 
+    		indexArray[index++] = 5; 
+    		indexArray[index++] = 1 + i % 4;
+    		indexArray[index++] = 1 + (1 + i) % 4; 
     	}
-    	for(int i = 0; i < 4; i++) {
-    		indexArray[index++] = 23; 
-    		indexArray[index++] = 19 + (0 + i) % 4;
-    		indexArray[index++] = 19 + (1 + i) % 4;  
+    	for(int i = 0; i < 6; i++) {
+    		indexArray[index++] = 18 + i * 3; 
+    		indexArray[index++] = 19 + i * 3; 
+    		indexArray[index++] = 20 + i * 3; 
+    		indexArray[index++] = 19 + i * 3; 
+    		indexArray[index++] = 20 + i * 3; 
+    		indexArray[index++] = 20 + i * 3; 
     	}
+    	vertexArray = changeZ(0.3f, vertexArray);
     }
     
     private float[] createLeg(float offset_x, float offset_z) {
     	float[] legArray = new float[9];
-    	legArray[0] = pos_x - offset_x;
-    	legArray[1] = ground;
-    	legArray[2] = pos_z + offset_z;
-    	legArray[3] = pos_x - offset_x * 2 / 3;
-    	legArray[4] = ground + 0.1f;
-    	legArray[5] = pos_z + offset_z;
-    	legArray[6] = pos_x - offset_x * 1 / 3;
-    	legArray[7] = ground + overGround + bodySize;
-    	legArray[8] = pos_z + offset_z;
+    	legArray[0] = offset_x;
+    	legArray[1] = 0;
+    	legArray[2] = offset_z;
+    	legArray[3] = 2 * offset_x;
+    	legArray[4] = 1;
+    	legArray[5] = offset_z;
+    	legArray[6] = 3 * offset_x;
+    	legArray[7] = -1;
+    	legArray[8] = offset_z;
     			
 		return legArray;
     }
     
-    private float[] createBody(float offset_x) {
-    	float[] bodyArray = new float[18];
-    	int index = 0;
-    	bodyArray[index++] = pos_x;
-    	bodyArray[index++] = ground + overGround;
-    	bodyArray[index++] = pos_z;
-		bodyArray[index++] = pos_x - offset_x * 1 / 3;
-		bodyArray[index++] = ground + overGround + bodySize;
-		bodyArray[index++] = pos_z - offset_x * 1 / 3;
-		bodyArray[index++] = pos_x - offset_x * 1 / 3;
-		bodyArray[index++] = ground + overGround + bodySize;
-		bodyArray[index++] = pos_z + offset_x * 1 / 3;
-		bodyArray[index++] = pos_x + offset_x * 1 / 3;
-		bodyArray[index++] = ground + overGround + bodySize;
-		bodyArray[index++] = pos_z + offset_x * 1 / 3;
-		bodyArray[index++] = pos_x + offset_x * 1 / 3;
-		bodyArray[index++] = ground + overGround + bodySize;
-		bodyArray[index++] = pos_z - offset_x * 1 / 3;
-    	bodyArray[index++] = pos_x;
-    	bodyArray[index++] = ground + overGround + 2 * bodySize;
-    	bodyArray[index++] = pos_z;
-    	
-		return bodyArray;
+    private float[] changeZ(float offset_z, float[] vertex_segment) {
+    	for(int i = 2; i < vertex_segment.length; i += 3) {
+    		vertex_segment[i] += offset_z;
+    	}
+		return vertex_segment;
     }
+
 }
